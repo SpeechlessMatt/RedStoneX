3a1289ba960d60c0707343a69dc9ffe3714ea0fe
czy4201_b <speechlessmatt@qq.com>
2026-07-06 15:22:31 +0800

    exposed destory simulator func to header

--- include/redstonex_sim.h
+++ include/redstonex_sim.h
@@ -82,6 +82,7 @@ void rsx_simulator_append_deque(RSXSimulator* sim, RSXConnectiveObject* target, 
 void rsx_simulator_schedule_source(RSXSimulator* sim, RSXConnectiveObject* source, uint32_t delay);
 
 RSXSimulator* rsx_create_simulator();
+void rsx_destroy_simulator(RSXSimulator* sim);
 void rsx_simulator_set_log_callback(RSXSimulator* sim, RSXLogCallback cb, void* user_data);
 void rsx_simulator_bind_object(RSXSimulator* sim, RSXConnectiveObject* obj);
 void rsx_simulator_run(RSXSimulator* sim);
